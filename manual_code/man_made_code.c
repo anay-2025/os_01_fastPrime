5986d57705c90f8a7d5f5d84df645d908beea9b2
roysamamdevs <sroy.frenzied@gmail.com>
2026-02-08 19:41:25 +0530

    Update man_made_code.c
    Include necessary headers and modular functions

--- manual_code/man_made_code.c
+++ manual_code/man_made_code.c
@@ -1,4 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <math.h>
+#include <time.h>
+#include <stdlib.h>
 
+// Prime check
+int isPrime(int n) {
+    if (n < 2) return 0;
+    for (int i=2; i*i<= n; i++) {
+        if (n % i == 0) return 0;
+    }
+    return 1;
+}
 
 //main section 
 int main() {
